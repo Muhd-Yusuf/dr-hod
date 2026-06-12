68c2ace3f5a6e142169c92b4cc5ea722b5b7c7ab
Muhd-Yusuf <office@blessoftware.com>
2026-06-12 23:07:55 +0100

    deploy: image basePath fix

---
+++ _next/static/hf9uWFn9TpKR0KwffjFoA/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/dr-hod//_next/:path+",
+        "destination": "/dr-hod/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
